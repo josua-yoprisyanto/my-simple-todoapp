228c8630297d7b301d2160b3a3ac43b665bb2dac
JY-GitTech <josua.tech@gmail.com>
2022-07-01 00:01:08 +0700

    navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,35 +1,89 @@
-import { signOut } from 'firebase/auth'
 import React, { useEffect, useState } from 'react'
 import { Navbar, Container } from 'react-bootstrap'
+import "../asset/css/navbar.css"
+import { Link } from 'react-scroll'
 import { useNavigate } from 'react-router-dom'
 import { auth } from '../firebase'
-import "../asset/css/navbar.css"
+import { signOut } from 'firebase/auth'
 
 const NavigationBar = () => {
 
+    const [changeButton, setChangeButton] = useState(false)
     const navigate = useNavigate()
 
-    const handleLogout = () => {
-        signOut(auth)
-            .then(() => {
-                navigate("/")
-            })
-            .catch(err => { alert(err.message) })
+    const handleRedirect = () => {
+        navigate("/auth")
     }
 
-    return (
-        <Navbar variant="dark" fixed='top'>
-            <Container>
-                <Navbar.Brand><strong className="navbar-title">Simple Todoapp</strong></Navbar.Brand>
-                <Navbar.Toggle />
+    const handleWelcome = () => {
+        navigate("/")
+    }
+
+    const handleSignOut = async() => {
+        await signOut(auth)
+        navigate("/auth")
+    }
+
+useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+        if (user) {
+            setChangeButton(true)
+        } else if(!user){
+            setChangeButton(false)
+        }
+    })
+}, [])
+
+return (
+    <Navbar variant="dark" bg="dark" sticky="top" expand="lg">
+        <Container>
+            <Navbar.Brand><strong className="navbar-title">My Simple Todoapp</strong></Navbar.Brand>
+            <Navbar.Toggle />
+            {changeButton
+                ?
                 <Navbar.Collapse className="justify-content-end">
                     <Navbar.Text>
-                        <button className='btn btn-outline-light navbar-button' onClick={handleLogout}>Logout</button>
+                        <li className='signin-button'>
+                            <button onClick={handleSignOut}>
+                                Sign Out
+                            </button>
+                        </li>
                     </Navbar.Text>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
+                </Navbar.Collapse >
+                :
+                <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Text>
+                        <li>
+                            <Link to="home" spy={true} smooth={true} offset={50} duration={500} onClick={handleWelcome}>
+                                Home
+                            </Link>
+                        </li>
+                    </Navbar.Text>
+                    <Navbar.Text>
+                        <li>
+                            <Link to="about" spy={true} smooth={true} offset={50} duration={500} onClick={handleWelcome}>
+                                About
+                            </Link>
+                        </li>
+                    </Navbar.Text>
+                    <Navbar.Text>
+                        <li>
+                            <Link to="contact" spy={true} smooth={true} offset={50} duration={500} onClick={handleWelcome}>
+                                Contact
+                            </Link>
+                        </li>
+                    </Navbar.Text>
+                    <Navbar.Text>
+                        <li className='signin-button'>
+                            <button onClick={handleRedirect}>
+                                Get Started
+                            </button>
+                        </li>
+                    </Navbar.Text>
+                </Navbar.Collapse >}
+        </Container >
+    </Navbar >
+)
 }
 
 export default NavigationBar
