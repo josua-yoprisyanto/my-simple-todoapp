cdd318d0afca89421f88b58994b994c99e32e92f
josua-yoprisyanto <josua.y8@gmail.com>
2023-01-16 20:44:54 +0700

    fix google & typo

--- src/components/welcome/WelcomeContent.js
+++ src/components/welcome/WelcomeContent.js
@@ -1,79 +1,106 @@
-import React from 'react'
-import "../../asset/css/welcome.css"
-import secureImage from '../../asset/images/secure.png'
-import simpleImage from '../../asset/images/simple.png'
-import learnImage from '../../asset/images/learn.png'
+import React from "react";
+import "../../asset/css/welcome.css";
+import secureImage from "../../asset/images/secure.png";
+import simpleImage from "../../asset/images/simple.png";
+import learnImage from "../../asset/images/learn.png";
 
 const WelcomeContent = () => {
-    return (
-        <div className='about mt-5 mb-5 container' id="about">
-            <h1 className='mb-3'><strong>About</strong></h1>
-            <span>My Simple Todoapp is an app that will help you organize your work and life easier.</span>
-            <span>We will keep make this app better and user friendly</span>
-            <span className='line mt-5 mb-5' />
-            <div className='positive'>
-                <div className='positive-text'>
-                    <h3>We are priority your privacy</h3>
-                    <p>we make sure every task you make will be secure by your user authentication, so other people can't access your task</p>
-                </div>
-                <div className='positive-img'>
-                    <img alt="secure" src={secureImage} />
-                </div>
-            </div>
-            <div className='positive'>
-                <div className='positive-img'>
-                    <img alt="simple" src={simpleImage} />
-                </div>
-                <div className='positive-text'>
-                    <h3>We are priority the way you organize</h3>
-                    <p>we make sure every task you make will easy view and make your work and life easier</p>
-                </div>
-            </div>
-            <div className='positive'>
-                <div className='positive-text'>
-                    <h3>We are priority your experience</h3>
-                    <p>we make sure everything you do here is easy to learn and easy to use</p>
-                </div>
-                <div className='positive-img'>
-                    <img alt="learn" src={learnImage} />
-                </div>
-            </div>
-            <div className='contact mt-5 mb-5' id="contact">
-                <div className='contact-information'>
-                    <h1><strong>Contact Information</strong></h1>
-                    <p>Feel free to contact us for any suggestion or problem</p>
-                    <span className='line mt-2 mb-2' />
-                    <a>
-                        <img alt="email" src="https://img.icons8.com/ios-glyphs/color/40/000000/new-post.png" />
-                        <span className='social'>Email</span>
-                    </a>
-                    <a>
-                        <img alt="instagram" src="https://img.icons8.com/ios-glyphs/color/40/000000/instagram-new--v1.png" />
-                        <span className='social'>Instagram</span>
-                    </a>
-                    <a>
-                        <img alt="github" src="https://img.icons8.com/ios-glyphs/40/000000/github.png" />
-                        <span className='social'>Github</span>
-                    </a>
-                    <a>
-                        <img alt="linkedin" src="https://img.icons8.com/ios-glyphs/40/000000/linkedin.png" />
-                        <span className='social'>Linkedin</span>
-                    </a>
-                </div>
-                <form className='contact-form'>
-                    <label htmlFor='name'>Name :</label>
-                    <input type="text" className='form-control' />
-                    <label htmlFor='email'>Email :</label>
-                    <input type="email" className="form-control" />
-                    <label htmlFor='subject'>Subject :</label>
-                    <input type="text" className='form-control' />
-                    <label htmlFor='message'>Message :</label>
-                    <textarea className='form-control' />
-                    <button className='btn btn-outline-dark'>Send Message</button>
-                </form>
-            </div>
+  return (
+    <div className="about mt-5 mb-5 container" id="about">
+      <h1 className="mb-3">
+        <strong>About</strong>
+      </h1>
+      <span>
+        My Simple Todoapp is an app that will help you organize your work and
+        life easier.
+      </span>
+      <span>We will keep making this app better and user friendly</span>
+      <span className="line mt-5 mb-5" />
+      <div className="positive">
+        <div className="positive-text">
+          <h3>We prioritize your privacy</h3>
+          <p>
+            we make sure every task you make will be secured by your user
+            authentication, so other people can't access your tasks
+          </p>
         </div>
-    )
-}
+        <div className="positive-img">
+          <img alt="secure" src={secureImage} />
+        </div>
+      </div>
+      <div className="positive">
+        <div className="positive-img">
+          <img alt="simple" src={simpleImage} />
+        </div>
+        <div className="positive-text">
+          <h3>We are prioritize the way you organize</h3>
+          <p>
+            we make sure every task you make will have a simple view and make
+            your work and life easier
+          </p>
+        </div>
+      </div>
+      <div className="positive">
+        <div className="positive-text">
+          <h3>We are prioritze your experience</h3>
+          <p>
+            we make sure everything you do here is easy to learn and easy to use
+          </p>
+        </div>
+        <div className="positive-img">
+          <img alt="learn" src={learnImage} />
+        </div>
+      </div>
+      <div className="contact mt-5 mb-5" id="contact">
+        <div className="contact-information">
+          <h1>
+            <strong>Contact Information</strong>
+          </h1>
+          <p>Feel free to contact us for any suggestion or problem</p>
+          <span className="line mt-2 mb-2" />
+          <a>
+            <img
+              alt="email"
+              src="https://img.icons8.com/ios-glyphs/color/40/000000/new-post.png"
+            />
+            <span className="social">Email</span>
+          </a>
+          <a>
+            <img
+              alt="instagram"
+              src="https://img.icons8.com/ios-glyphs/color/40/000000/instagram-new--v1.png"
+            />
+            <span className="social">Instagram</span>
+          </a>
+          <a>
+            <img
+              alt="github"
+              src="https://img.icons8.com/ios-glyphs/40/000000/github.png"
+            />
+            <span className="social">Github</span>
+          </a>
+          <a>
+            <img
+              alt="linkedin"
+              src="https://img.icons8.com/ios-glyphs/40/000000/linkedin.png"
+            />
+            <span className="social">Linkedin</span>
+          </a>
+        </div>
+        <form className="contact-form">
+          <label htmlFor="name">Name :</label>
+          <input type="text" className="form-control" />
+          <label htmlFor="email">Email :</label>
+          <input type="email" className="form-control" />
+          <label htmlFor="subject">Subject :</label>
+          <input type="text" className="form-control" />
+          <label htmlFor="message">Message :</label>
+          <textarea className="form-control" />
+          <button className="btn btn-outline-dark">Send Message</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default WelcomeContent
+export default WelcomeContent;
--- src/pages/Authentication.js
+++ src/pages/Authentication.js
@@ -1,121 +1,152 @@
-import React, { useEffect, useState } from 'react'
-import '../asset/css/auth.css'
-import { GoogleButton } from 'react-google-button'
+import React, { useEffect, useState } from "react";
+import "../asset/css/auth.css";
+import { GoogleButton } from "react-google-button";
 import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   GoogleAuthProvider,
-  signInWithRedirect,
-} from 'firebase/auth'
-import { auth } from '../firebase'
-import { useNavigate } from 'react-router-dom'
-import auth1Image from '../asset/images/auth1.png'
-import auth2Image from '../asset/images/auth2.png'
-import Navbar from '../components/NavigationBar'
+  signInWithPopup,
+} from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
+import auth1Image from "../asset/images/auth1.png";
+import auth2Image from "../asset/images/auth2.png";
+import Navbar from "../components/NavigationBar";
 
 const Authentication = () => {
-
-  const [changeForm, setChangeForm] = useState(true)
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [confirmPassword, setConfirmPassword] = useState("")
-  const [isError, setIsError] = useState(false)
-  const [errorMessage, setErrorMessage] = useState("")
-  const navigate = useNavigate()
+  const [changeForm, setChangeForm] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isError, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
 
   const handleSignIn = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      await signInWithEmailAndPassword(auth, email, password)
-      navigate("/dashboard")
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/dashboard");
     } catch (error) {
-      setIsError(true)
-      setErrorMessage("Login Failed")
+      setIsError(true);
+      setErrorMessage("Login Failed");
     }
-  }
+  };
 
   const handleSignUp = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (password !== confirmPassword) {
-      setIsError(true)
-      setErrorMessage("Password and Confirm Password Not Same")
-      return
+      setIsError(true);
+      setErrorMessage("Password and Confirm Password Not Same");
+      return;
     } else if (email === "" || password === "" || confirmPassword === "") {
-      setIsError(true)
-      setErrorMessage("Email/Password Empty")
+      setIsError(true);
+      setErrorMessage("Email/Password Empty");
     }
     try {
-      await createUserWithEmailAndPassword(auth, email, password)
-      navigate("/dashboard")
+      await createUserWithEmailAndPassword(auth, email, password);
+      navigate("/dashboard");
     } catch (error) {
-      setIsError(true)
-      setErrorMessage("Account Already Exist")
+      setIsError(true);
+      setErrorMessage("Account Already Exist");
     }
-  }
+  };
 
   const signInWithGoogle = async () => {
-    const provider = new GoogleAuthProvider()
-    await signInWithRedirect(auth, provider)
-    navigate("/dashboard")
-  }
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(auth, provider);
+    navigate("/dashboard");
+  };
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       if (user) {
-        navigate("/dashboard")
+        navigate("/dashboard");
       }
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <>
       <Navbar />
-      <div className='auth-container'>
-        <div className='auth-img'>
+      <div className="auth-container">
+        <div className="auth-img">
           <img src={auth2Image} />
         </div>
-        {changeForm ?
-          <div div className='auth-form'>
-            <h2><strong>Sign In</strong></h2>
-            {isError && <span className='mt-2 mb-2' style={{ color: "red" }}>{errorMessage}</span>}
-            <GoogleButton onClick={signInWithGoogle} style={{ width: "300px", color: "black", backgroundColor: "white" }} />
-            <form className='mt-3' onSubmit={handleSignIn}>
+        {changeForm ? (
+          <div div className="auth-form">
+            <h2>
+              <strong>Sign In</strong>
+            </h2>
+            {isError && (
+              <span className="mt-2 mb-2" style={{ color: "red" }}>
+                {errorMessage}
+              </span>
+            )}
+            <GoogleButton
+              onClick={signInWithGoogle}
+              style={{
+                width: "300px",
+                color: "black",
+                backgroundColor: "white",
+              }}
+            />
+            <form className="mt-3" onSubmit={handleSignIn}>
               <input
                 type="email"
                 className="form-control"
-                placeholder='Email'
+                placeholder="Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
               <input
                 type="password"
                 className="form-control"
-                placeholder='Password'
+                placeholder="Password"
                 minLength="8"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
-              <button className='btn btn-dark mb-3'>Sign In</button>
+              <button className="btn btn-dark mb-3">Sign In</button>
             </form>
-            <span>Don't have account? <a onClick={() => setChangeForm(false)}><strong>Sign Up</strong></a> Here</span>
+            <span>
+              Don't have account?{" "}
+              <a onClick={() => setChangeForm(false)}>
+                <strong>Sign Up</strong>
+              </a>{" "}
+              Here
+            </span>
           </div>
-          :
-          <div className='auth-form'>
-            <h2><strong>Sign Up</strong></h2>
-            {isError && <span className='mt-2 mb-2' style={{ color: "red" }}>{errorMessage}</span>}
-            <GoogleButton onClick={signInWithGoogle} style={{ width: "300px", color: "black", backgroundColor: "white" }} />
-            <form className='mt-3 mb-2' onSubmit={handleSignUp}>
+        ) : (
+          <div className="auth-form">
+            <h2>
+              <strong>Sign Up</strong>
+            </h2>
+            {isError && (
+              <span className="mt-2 mb-2" style={{ color: "red" }}>
+                {errorMessage}
+              </span>
+            )}
+            <GoogleButton
+              onClick={signInWithGoogle}
+              style={{
+                width: "300px",
+                color: "black",
+                backgroundColor: "white",
+              }}
+            />
+            <form className="mt-3 mb-2" onSubmit={handleSignUp}>
               <input
                 type="email"
                 className="form-control"
-                placeholder='Email'
+                placeholder="Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
               <input
                 type="password"
                 className="form-control"
-                placeholder='Password'
+                placeholder="Password"
                 minLength="8"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -123,22 +154,28 @@ const Authentication = () => {
               <input
                 type="password"
                 className="form-control"
-                placeholder='Confirm Password'
+                placeholder="Confirm Password"
                 minLength="8"
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
               />
-              <button className='btn btn-dark mb-2'>Sign Up</button>
+              <button className="btn btn-dark mb-2">Sign Up</button>
             </form>
-            <span>Already have account? <a onClick={() => setChangeForm(true)}><strong>Sign In</strong></a> Here</span>
+            <span>
+              Already have account?{" "}
+              <a onClick={() => setChangeForm(true)}>
+                <strong>Sign In</strong>
+              </a>{" "}
+              Here
+            </span>
           </div>
-        }
-        <div className='auth-img'>
+        )}
+        <div className="auth-img">
           <img src={auth1Image} />
         </div>
-      </div >
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Authentication
+export default Authentication;
